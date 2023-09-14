e20231d7e2ca61bf782bad80213182e7914343e9
Tanish Mohanta <tanishmohanta1901@gmail.com>
2023-09-14 18:21:47 +0530

    PIC initialization completed.

--- src/kernel.asm
+++ src/kernel.asm
@@ -22,6 +22,27 @@ _start:
     or al, 2
     out 0x92, al
 
+    ;initialization process for remapping the master Programmable Interrupt Controller (PIC)
+    ;ICWs are sent to the PICs' command ports (0x20 for master PIC and 0xA0 for slave PIC in x86 architecture) during the initialization process. These control words help set up the PICs to properly handle hardware interrupts and to avoid conflicts in the interrupt handling process.
+    ; Remap the master PIC
+
+    ; Set ICW1 (Initialization Control Word 1) for the master PIC
+    mov al, 00010001b   ; Load binary value 00010001b into AL
+    out 0x20, al        ; Send the value in AL to port 0x20 (master PIC command port)
+
+    ; Set ICW2 (Initialization Control Word 2) for the master PIC
+    mov al, 0x20        ; Load the value 0x20 into AL
+    out 0x21, al        ; Send the value in AL to port 0x21 (master PIC data port)
+    ;This value represents the interrupt vector offset. It's the value at which the master PIC will start delivering interrupts. In this case, it's 0x20, which corresponds to interrupt vector 0x20 (32 in decimal).
+    ;32-255
+
+    ; Set ICW3 (Initialization Control Word 3) for the master PIC
+    mov al, 00000001b   ; Load binary value 00000001b into AL
+    out 0x21, al        ; Send the value in AL to port 0x21 (master PIC data port)
+
+    ; End of remap of the master PIC
+
+
     call kernel_main
 
     jmp $
